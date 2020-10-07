5a1ab674b626989d7577adfba468f7ac9c736f93
Nosov-Pavel <nopaev@gmail.com>
2020-10-06 22:27:58 -0400

    addNewCounters

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counters, setCounters] = useState(['counter1', 'counter2', 'counter3']);
+
+    const addCounter=() => {
+        const arr = [...counters];
+        const element = 'counter' + arr.length;
+        arr.push(element);
+        setCounters(arr)
+    }
+
+    return (
+        <div className='App'>
+            <button onClick={addCounter}>AddNewCounters</button>
+            {counters.map(el => <li>{el}</li>)}
+
+        </div>
+    );
 }
 
 export default App;
